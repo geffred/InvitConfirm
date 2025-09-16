aad8520cdbb3e66edefa6c888553d302419a1b13
geffred <tchapda20@gmail.com>
2025-09-16 12:12:41 +0200

    Initialisation du projet

---
+++ src/main/java/com/invitation/confirmation/ConfirmationApplication.java
@@ -0,0 +1,13 @@
+package com.invitation.confirmation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfirmationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfirmationApplication.class, args);
+	}
+
+}
